8d097c91c3822baf6f65eef1bdc73dacea595ab2
mostafa elaanbari <mostafaelaanbari@gmail.com>
2024-04-01 15:20:18 +0000

    Add Specification repository

---
+++ Infrastructure/Specifications/Specification.cs
@@ -0,0 +1,28 @@
+﻿using Domain.Primitives;
+using System.Linq.Expressions;
+
+namespace Infrastructure.Specifications;
+
+internal abstract class Specification<TEntity> where TEntity : Entity
+{
+    private Expression<Func<TEntity, bool>>? Cretaria { get; }
+    private bool _orderByCalled = false;
+
+    protected Specification(Expression<Func<TEntity, bool>>? cretaria)
+        => Cretaria = cretaria;
+
+    public List<Expression<Func<TEntity, object>>> Conditions { get; } = new();
+    public SpecificationOrdering<TEntity>? SpecificationOrdering { get; private set; }
+
+    protected void AddInclude(Expression<Func<TEntity, object>> condition) => Conditions.Add(condition);
+    protected SpecificationOrdering<TEntity> OrderBy(Expression<Func<TEntity, object>> orderFunction)
+    {
+        if (_orderByCalled)
+        {
+            throw new InvalidOperationException("AddOrderBy can only be called once.");
+        }
+
+        _orderByCalled = true;
+        return SpecificationOrdering = new(orderFunction);
+    }
+}
---
+++ Infrastructure/Specifications/SpecificationOrdering.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq.Expressions;
+
+namespace Infrastructure.Specifications;
+
+internal class SpecificationOrdering<TEntity>
+{
+    public List<Expression<Func<TEntity, object>>> Ordering { get; } = new();
+
+    public SpecificationOrdering(Expression<Func<TEntity, object>> orderFunction)
+        => Ordering.Add(orderFunction);
+
+    public SpecificationOrdering<TEntity> ThenBy(Expression<Func<TEntity, object>> orderFunction)
+    {
+        Ordering.Add(orderFunction);
+        return this;
+    }
+}
