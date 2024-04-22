aabb6292360bf8ea63c8d614454e7b0194437261
mostafa elaanbari <mostafaelaanbari@gmail.com>
2024-04-22 12:24:38 +0100

    Add transaction on UnitOfWork pattern

--- Domain/Repositories/IUnitOfWork.cs
+++ Domain/Repositories/IUnitOfWork.cs
@@ -1,6 +1,18 @@
-﻿namespace Domain.Repositories;
+﻿using System.Data;
+
+namespace Domain.Repositories;
 
 public interface IUnitOfWork
 {
+    /// <summary>
+    /// save change async to data base
+    /// </summary>
+    /// <param name="cancellationToken"></param>
+    /// <returns>Task</returns>
     Task SaveChangesAsync(CancellationToken cancellationToken = default);
+    /// <summary>
+    /// Create a transaction
+    /// </summary>
+    /// <returns>IDbTransaction</returns>
+    IDbTransaction BeginTransaction();
 }
--- Infrastructure/Repositories/UnitOfWork.cs
+++ Infrastructure/Repositories/UnitOfWork.cs
@@ -1,10 +1,19 @@
-﻿namespace Infrastructure.Repositories;
+﻿using Microsoft.EntityFrameworkCore.Storage;
+using System.Data;
+
+namespace Infrastructure.Repositories;
 
 public sealed class UnitOfWork : IUnitOfWork
 {
     private readonly ApplicationDbContext _dbContext;
 
     public UnitOfWork(ApplicationDbContext dbContext) => _dbContext = ThrowIfNull(dbContext);
+
+    public IDbTransaction BeginTransaction()
+    {
+        var transaction = _dbContext.Database.BeginTransaction();
+        return transaction.GetDbTransaction();
+    }
 
     public Task SaveChangesAsync(CancellationToken cancellationToken = default) => _dbContext.SaveChangesAsync(cancellationToken);
 }
